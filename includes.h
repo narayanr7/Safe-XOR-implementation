b237cbff276a201f6a43becae866caf33a051c61
rek1 <rekarger@gmail.com>
2017-01-24 19:56:10 -0500

    Update includes.h

--- includes.h
+++ includes.h
@@ -3,16 +3,9 @@
 
 #include <openssl/bio.h>
 #include <openssl/evp.h>
-#include <stdlib.h>
 #include <iostream>
 #include <boost/algorithm/string.hpp>
 #include <string>
 #include <cstring>
-#define red "\e[31m"
-#define light_blue "\e[95m"
-#define light_red "\e[91m"
-#define yellow "\e[33m"
-#define cyan "\e[36m"
-#define normal "\e[0m"
 
 #endif // INCLUDES_H
